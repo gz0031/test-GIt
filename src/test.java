10d717dbbc2b2f4be268178e35d4665d76842a1b
高泽 <1132241600@qq.com>
2021-04-11 16:40:44 +0800

    sss

--- src/test.java
+++ src/test.java
@@ -1,6 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("version1");
-        System.out.println("dev");
+        System.out.println("master");
     }
 }
