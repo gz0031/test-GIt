0ba1ebd4d2f5647aa90bd49eb5d17c62f562173c
高泽 <1132241600@qq.com>
2021-04-11 16:20:47 +0800

    dev补充

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("version1");
         System.out.println("dev");
+        System.out.println("补充");
     }
 }
